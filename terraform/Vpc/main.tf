5b7cfdfa3cfd078c0ddf05e32176448d9b962169
Sagar <rohit28sagar@gmail.com>
2026-09-18 12:02:39 +0530

    fix: ignore terraform providers and heavy binaries

---
+++ terraform/Vpc/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+  source               = "./modules/vpc"
+  project_name         = "prod-app"
+  vpc_cidr             = var.vpc_cidr
+  public_subnet_cidrs  = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  availability_zones   = var.availability_zones
+}
